4d1b4b2542758f86d18cce6acc70cb71c4e1c213
Pavan Hegde <pavan.hegde@rocketmail.com>
2014-03-24 23:11:34 -0400

    Updating E'er'one on the Code Again. (3/24) Main additions include new Autonomous Routines such as Ethyl and Butyl; it seems like we skipped Propyl. This is the code used at Lenape which cause the driver to experience some lag, due to speculation, SmartDashboard was disabled temporarily, RoboRealm was closed on the driver station, but luckily a threading problem was caught and fixed. Supposedly after all of these changes there is still some lag being experienced.

--- Aerial_Assist/src/org/firstrobotics1923/AssistRobot.java
+++ Aerial_Assist/src/org/firstrobotics1923/AssistRobot.java
@@ -1,7 +1,6 @@
 package org.firstrobotics1923;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import org.firstrobotics1923.event.CompressorOffEvent;
 import org.firstrobotics1923.event.CompressorOnEvent;
@@ -14,24 +13,30 @@ import org.firstrobotics1923.event.ShooterLowerAngleEvent;
 import org.firstrobotics1923.event.ShooterRaiseAngleEvent;
 import org.firstrobotics1923.event.ShooterStartEvent;
 import org.firstrobotics1923.event.ShooterStopEvent;
-import org.firstrobotics1923.routines.AutonomousRoutine;
+import org.firstrobotics1923.routines.ButylRoutine;
+import org.firstrobotics1923.routines.EthylRoutine;
 import org.firstrobotics1923.routines.MethylRoutine;
-import org.firstrobotics1923.routines.MethylRoutine2;
+//import org.firstrobotics1923.util.Dashboard;
 import org.firstrobotics1923.util.XboxController;
+//import org.firstrobotics1923.Components;
+
 /**
  * The Core Code for FRC Team 1923's "Aerial Assist" Robot 
  * 
- * @author Pavan Hegde & whoever did the dashboard stuff that is commented out
- * @version 1.0
- * @since Feb. 15, 2014
+ * @author Pavan Hegde & whoever else touched this without commenting
+ * @version 1.3
+ * @since Mar. 11, 2014
  */
-public class AssistRobot extends IterativeRobot{
+public class AssistRobot extends IterativeRobot{ 
+    
     private XboxController xbc = Components.operatorControl;
     
     private boolean[] justPressed = new boolean[14];       //Array to store Xbox button input
     private boolean[] triggers = new boolean[2]; //Array to store Xbox trigger input
-        
-    private AutonomousRoutine auton;
+    
+    private double shooterReady = 2.5;
+    
+    private String currentAutonRoutine = "butyl";
 
     private boolean compressorOn = false;
     
@@ -39,12 +44,13 @@ public class AssistRobot extends IterativeRobot{
     
     public void robotInit(){
         this.table = Components.table;
-  //      Components.rightDriveEncoder.setDistancePerPulse(1); //TODO: update
-//        Components.leftDriveEncoder.setDistancePerPulse(1); //TODO: update
         this.compressorOn = false;
         Components.shooterAngleSystem.stop();
         Components.intakeSystem.stop();
+       // Components.sfxDashboard.initSDVariables();
+      //  Components.sfxDashboard.smartDashboardUpdate();
     }
+    
     
     /**
      * Ensures that all systems are disabled
@@ -60,30 +66,51 @@ public class AssistRobot extends IterativeRobot{
     public void disabledPeriodic() {
         //... ... ... ... ... Yes ... ... ...
     }
-    
+
     /**
      * Called once at the start of Auton
      */
     public void autonomousInit() {
-        MethylRoutine2.start();
-        //auton = new MethylRoutine();
-        //auton.run();
+      //  Components.sfxDashboard.smartDashboardUpdate();
+        if (currentAutonRoutine.equalsIgnoreCase("methyl")){ //UNUSABLE!!!!! ROBOREALM NO LONGER PASSES TARGET DISTANCE
+            MethylRoutine.start();
+        } else if (currentAutonRoutine.equalsIgnoreCase("ethyl")){
+            EthylRoutine.start();
+        } else if (currentAutonRoutine.equalsIgnoreCase("butyl")) {
+            ButylRoutine.initialize();
+        }
     }
     
     /**
      * Called periodically in auton
      */
-    public void autonomousPeriodic(){
-        MethylRoutine2.doEvent();
-       // EventBus.instance.clean();
-       // EventBus.instance.next();
+    public void autonomousPeriodic(){                
+        if (currentAutonRoutine.equalsIgnoreCase("methyl")){ //UNUSABLE!!!! ROBOREALM NO LONGER PASSED TARGET DISTANCE
+            MethylRoutine.routine();
+        } else if (currentAutonRoutine.equalsIgnoreCase("ethyl")){
+            EthylRoutine.routine();
+        } else if (currentAutonRoutine.equalsIgnoreCase("butyl")) {
+            ButylRoutine.routine();
+        }
+   // Components.sfxDashboard.smartDashboardUpdate();
     }
     
     /**
      * Initializes required things before teleop
      */
     public void teleopInit() {
-        MethylRoutine2.stop();
+        if (currentAutonRoutine.equals("methyl")){     //DO NOT USE METHYL!!!! NO MORE TARGET DISTANCE... No point in it
+            MethylRoutine.stop();
+        } else if (currentAutonRoutine.equalsIgnoreCase("ethyl")){
+            EthylRoutine.stop();
+        } else if (currentAutonRoutine.equalsIgnoreCase("butyl")) {
+            ButylRoutine.stop();
+        }
+      //  Components.sfxDashboard.smartDashboardUpdate();
+        if (Components.sfxDashboard.useDashboardVar){
+            shooterReady = Components.sfxDashboard.var_1Value;
+        }
+        
         EventBus.instance.clear();        
     }
     
@@ -91,35 +118,10 @@ public class AssistRobot extends IterativeRobot{
      * All of the periodically called teleop-functions (eg. input)
      */
     public void teleopPeriodic() {
-        { //Driving Scope
-                    System.out.println("Hot Target " + table.getString("Hot_Target"));
-                    System.out.println("Distance: " + table.getNumber("TARGET_DISTANCE"));
-
-            double idealLeft = Components.leftStick.getCoalescedY();
-            double idealRight = Components.rightStick.getCoalescedY();
-
- //           System.out.println("Encoder Rate L, R: " + Components.leftDriveEncoder.getRate() + ", " + Components.rightDriveEncoder.getRate());
- //           double realLeft = Components.leftDriveEncoder.getRate() / 1024; //TODO Update Values to Rate at 1.0
- //           double realRight = Components.rightDriveEncoder.getRate() / 1024; //TODO Update Values to Rate at 1.0
-            
-  //          double errorLeft = realLeft - idealLeft;
-  //          double errorRight = realRight - idealRight;
-
-            double newLeft = idealLeft;// - errorLeft;
-            double newRight = idealRight;// - errorRight;
-
-            double maxNewLeftRight = Math.max(1, Math.max(Math.abs(newLeft), Math.abs(newRight)));
-            
-            newLeft = newLeft / maxNewLeftRight;
-            newRight = newRight / maxNewLeftRight;            
-            
-           Components.robotDrive.drive(newLeft, newRight);
+        { //Driving Scope : Currently Full Joystick Control, no correction
+            Components.robotDrive.drive(Components.leftStick.getCoalescedY(), Components.rightStick.getCoalescedY());
         } //End Driving Scope
-        
-        {//Targeting Scope
-            //System.out.println("Distance: " + table.getNumber("/SmartDashboard/Distance"));
-        } //End Targeting Scope
-        
+     
         {  //Shooter Scope
            if (Components.operatorControl.getButton(XboxController.Button.Start) & !justPressed[XboxController.Button.Start.value]) {    //Start button turns on shooter
                EventBus.instance.push(new ShooterStartEvent());
@@ -134,10 +136,15 @@ public class AssistRobot extends IterativeRobot{
            } else {
                justPressed[XboxController.Button.Back.value] = false;
            }
+           
+//           if (Components.shooterSystem.getShootTime() >= shooterReady) {
+//               Components.sfxDashboard.CompressorRelay = true;
+//           } else {
+//               Components.sfxDashboard.CompressorRelay = false;
+//           }
         } //End Shooter Scope
         
         { // Shooter Angle Scope
-           //System.out.println("Triggers: " + Components.operatorControl.getRawAxis(3)); //TODO DEBUG
            if ((Components.operatorControl.getRawAxis(3) < 0) && !triggers[0]) {         //Left Trigger lowers the shooter angle
                EventBus.instance.push(new ShooterLowerAngleEvent());
                triggers[0] = true;
@@ -158,6 +165,7 @@ public class AssistRobot extends IterativeRobot{
            if (Components.operatorControl.getButton(XboxController.Button.B) & !justPressed[XboxController.Button.B.value]) {         //B angles intake in
                EventBus.instance.push(new IntakeAngleInEvent());
                justPressed[XboxController.Button.B.value] = true;
+               Components.shooterSystem.resetTimer();
            } else {
                justPressed[XboxController.Button.B.value] = false;
            }
@@ -190,29 +198,14 @@ public class AssistRobot extends IterativeRobot{
                justPressed[XboxController.Button.RB.value] = false;
            }           
         } //End Intake Scope
-        /*if (Components.operatorControl.getButton(XboxController.Button.A)) {         //B angles intake in
-              // EventBus.instance.push(new CompressorOnEvent());
-             
-               System.out.println("A button pressed and just pressed: ");
-               Components.compressorSpike.set(Relay.Value.kForward);
-               System.out.println("Relay Value: " + Components.compressorSpike.get().value);
-               
-       } else {
-               Components.compressorSpike.set(Relay.Value.kOff);
-               System.out.println("Relay Value: " + Components.compressorSpike.get().value);
-        }*/
+        
         { //Compressor Scope
-            boolean safe = Components.compressorSafety.get();
-            System.out.println("Safety: " + safe);
-            if (!Components.compressorSafety.get() && !compressorOn) {
+            boolean safety = Components.compressorSafety.get();
+            if (!safety && !compressorOn) {
                 EventBus.instance.push(new CompressorOnEvent());
-                System.out.println("In here at least once");
-                System.out.println("IF1: " + safe);
                 compressorOn = true;
-            }else if (Components.compressorSafety.get() && compressorOn) {
+            }else if (safety && compressorOn) {
                 EventBus.instance.push(new CompressorOffEvent());
-                System.out.println("In other here at least once");
-                System.out.println("IF2: " + safe);
                 compressorOn = false;
             }
         } //End Compressor Scope
@@ -221,84 +214,13 @@ public class AssistRobot extends IterativeRobot{
             EventBus.instance.next();
             EventBus.instance.clean();
         } //End EvntBus Scope
-
-        //updateDashboard();
+        { //SFX Dashboard Scope
+           //sfxDashboard.smartDashboardUpdate();
+           // Components.sfxDashboard.smartDashboardUpdate();
+           
+        } // SFX End Dashboard Scope
+        //{ //Dashboard Scope
+           // Dashboard.update();
+        //} //End Dashboard Scope
     }
-    
-    /*public void updateDashboard() {
-        Dashboard lowDashData = DriverStation.getInstance().getDashboardPackerLow();
-        lowDashData.addCluster();
-        {
-            lowDashData.addCluster();
-            {     //analog modules
-                lowDashData.addCluster();
-                {
-                    for (int i = 1; i <= 8; i++) {
-                        lowDashData.addFloat((float) AnalogModule.getInstance(1).getAverageVoltage(i));
-                    }
-                }
-                lowDashData.finalizeCluster();
-                lowDashData.addCluster();
-                {
-                    for (int i = 1; i <= 8; i++) {
-                        lowDashData.addFloat((float) AnalogModule.getInstance(2).getAverageVoltage(i));
-                    }
-                }
-                lowDashData.finalizeCluster();
-            }
-            lowDashData.finalizeCluster();
-
-            lowDashData.addCluster();
-            { //digital modules
-                lowDashData.addCluster();
-                {
-                    lowDashData.addCluster();
-                    {
-                        int module = 1;
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getAllDIO());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getDIODirection());
-                        lowDashData.addCluster();
-                        {
-                            for (int i = 1; i <= 10; i++) {
-                                lowDashData.addByte((byte) DigitalModule.getInstance(module).getPWM(i));
-                            }
-                        }
-                        lowDashData.finalizeCluster();
-                    }
-                    lowDashData.finalizeCluster();
-                }
-                lowDashData.finalizeCluster();
-
-                lowDashData.addCluster();
-                {
-                    lowDashData.addCluster();
-                    {
-                        int module = 2;
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayReverse());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getAllDIO());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getDIODirection());
-                        lowDashData.addCluster();
-                        {
-                            for (int i = 1; i <= 10; i++) {
-                                lowDashData.addByte((byte) DigitalModule.getInstance(module).getPWM(i));
-                            }
-                        }
-                        lowDashData.finalizeCluster();
-                    }
-                    lowDashData.finalizeCluster();
-                }
-                lowDashData.finalizeCluster();
-
-            }
-            lowDashData.finalizeCluster();
-
-            lowDashData.addByte(Solenoid.getAllFromDefaultModule());
-        }
-        lowDashData.finalizeCluster(); 
-        lowDashData.commit();
-    }*/
 }
-
